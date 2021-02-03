d625d47efafae81442a6870f0c6970de6b636132
Elias Abunuwara <eliasabunuwara@eliass-mbp.mynet>
2021-02-03 18:54:10 -0500

    fixed login + register

--- who-prompt.py
+++ who-prompt.py
@@ -1,68 +1,50 @@
 from flask import Flask, redirect, url_for, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from flask_login import UserMixin
 from datetime import timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf import FlaskForm
-from wtforms import StringField, passwordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
+Bootstrap(app)
 # key for encryption
 app.secret_key = "tech4good"
+# setup database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite3"
+db = SQLAlchemy(app)
 
 
 class LoginForm(FlaskForm):
     username = StringField(
         "username", validators=[InputRequired(), Length(min=4, max=15)]
     )
-    password = passwordField(
+    password = PasswordField(
         "password", validators=[InputRequired(), Length(min=8, max=80)]
     )
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(
+        "username", validators=[InputRequired(), Length(min=4, max=15)]
+    )
+    password = PasswordField(
+        "password", validators=[InputRequired(), Length(min=8, max=80)]
+    )
+    email = StringField("email", validators=[InputRequired(), Length(max=50)])
 
 
 # stay logged in with most recent user for 5 minutes
 app.permanent_session_lifetime = timedelta(minutes=5)
 
-# setup database
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.sqlite3"
 
-db = SQLAlchemy(app)
-
-
-# class users(db.Model):
-#     __tablename__ = "flasklogin-users"
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100), nullable=False, unique=True)
-#     email = db.Column(db.String(100), nullable=False, unique=True)
-#     pasword = db.Column(db.String(100), nullable=False, unique=False)
-
-#     # create hashed password
-#     def set_password(self, password):
-#         self.password = generate_password_hash(password, method="sha256")
-
-#     # check hashed password
-#     def check_password(self, password):
-#         return check_password_hash(self.password, password)
-
-#     def __repr__(self):
-#         return "<User {}>".format(self.username)
-
-#     def __init__(self, name, email):
-#         self.name = name
-#         self.email = email
-
-
-class users(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(100), unique=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100), unique=False)
-
-    def __init__(self, name, email, password):
-        self.name = name
-        self.email = email
-        self.password = password
 
 
 # path to function home
@@ -76,34 +58,14 @@ def home():
 # define function
 def login():
     form = LoginForm()
-    if "user" in session:
-        user = session["user"]
-        return redirect(url_for("user"))
-    # receive the name passed to the input field
-    elif request.method == "POST":
-        session.permanent = True
-        user = request.form["nm"]
-        email = request.form["email"]
-        password = request.form["pwd"]
 
-        user_exists = users.query.filter_by(email=email).first()
-        # if user is new
-        if not user_exists:
-            new = users(name=user, email=email, password=password)
-            db.session.add(new)
-            db.session.commit()
-            session["user"] = user
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                return redirect(url_for("home"))
 
-            return redirect(url_for("user"))
-        # if user exists
-        else:
-            if user_exists and user_exists.password == password:
-                session["user"] = user
-            else:
-                flash("invalid user credentials")
-                return redirect(url_for("login"))
-    else:
-        return render_template("login.html")
+    return render_template("login.html", form=form)
 
 
 @app.route("/user", methods=["POST", "GET"])
@@ -125,6 +87,25 @@ def user():
         return render_template("user.html", email=email)
     else:
         return redirect(url_for("login"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method="sha256")
+        new_user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=hashed_password,
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        return "<h1>New user has been created </h1>"
+
+    return render_template("register.html", form=form)
 
 
 @app.route("/logout")
